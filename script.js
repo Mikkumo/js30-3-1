2eff67922152badfa9a644f77b0faf33f4cc15ea
Lissa <sokolovskaya_lisa@mail.ru>
2022-02-22 02:55:20 +0400

    feat: add function check

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const index = document.getElementById('index')
 let move = 0
+let result = ''
 
 index.addEventListener('click', e => {
     if (e.target.className = 'cell') {
@@ -9,3 +10,24 @@ index.addEventListener('click', e => {
     }
 })
 
+const check = () => {
+    let cells = document.getElementsByClassName('cell')
+    let winPosition = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    winPosition.forEach(e => {
+        if (cells[e][0].innerHTML === 'X' && cells[e][1].innerHTML === 'X' && cells[e][2].innerHTML === 'X') {
+            result = 'крестики'
+        } else if (cells[e][0].innerHTML === '0' && cells[e][1].innerHTML === '0' && cells[e][2].innerHTML === '0') {
+            result = 'нолики'
+        }
+    });
+}
